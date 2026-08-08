100fddd5a762441b07adfd8bc52404465bcb60c3
EmperorJake <68182631+EmperorJake@users.noreply.github.com>
2026-08-08 15:21:08 +1000

    make farm produce grain in basic

--- src/industries/farm.py
+++ src/industries/farm.py
@@ -41,7 +41,7 @@ industry.economy_variations["BASIC_TROPIC"].prod_cargo_types_with_multipliers = 
 industry.economy_variations["BASIC_TEMPERATE"].enabled = True
 industry.economy_variations["BASIC_TEMPERATE"].prob_map_gen = "14"
 industry.economy_variations["BASIC_TEMPERATE"].prod_cargo_types_with_multipliers = [
-    ("OLSD", 20),
+    ("GRAI", 20),
     ("LVST", 12),
 ]
 
